5aaae843409bff53fd6926117092c89af7b5095f
f1982 <andycao@yeah.net>
2022-08-10 10:34:09 +0000

    Deploying to gh-pages from @ f1982/desk-setup-editor@db3175abdb88e8bd291fec15f4a4f6dd07c78565 🚀

---
+++ precache-manifest.f1cadf32a36c6516c73faee923a7ad39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6892894db3eb918df9b",
+    "url": "/static/css/main.91d21c06.chunk.css"
+  },
+  {
+    "revision": "c6892894db3eb918df9b",
+    "url": "/static/js/main.2b50649d.chunk.js"
+  },
+  {
+    "revision": "6e5f10929bc98e43f994",
+    "url": "/static/js/runtime~main.9f0bb20b.js"
+  },
+  {
+    "revision": "db6673fd0b3ec2e07777",
+    "url": "/static/js/2.65453671.chunk.js"
+  },
+  {
+    "revision": "3557284d9ed1f22f8b0f",
+    "url": "/static/js/3.60259b98.chunk.js"
+  },
+  {
+    "revision": "ecf36c2333ced07cc03a",
+    "url": "/static/js/4.8f1548f6.chunk.js"
+  },
+  {
+    "revision": "4a227383900c4c50b66681965ec6ecf9",
+    "url": "/index.html"
+  }
+];
