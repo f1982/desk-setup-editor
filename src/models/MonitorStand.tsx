b71722888fa7751d952b335f774bcbdde5993389
Andy Cao <caojundan@gmail.com>
2022-08-28 19:55:42 +1200

    feat: add monitor stand as test

--- src/models/MonitorStand.tsx
+++ src/models/MonitorStand.tsx
@@ -1,4 +1,4 @@
-import { Box3, Vector3 } from 'three';
+import { Box3, Box3Helper, Color, Mesh, Object3D, Vector3 } from 'three';
 import DSEObject from './DSEObject';
 
 class MonitorStand extends DSEObject {
@@ -7,12 +7,41 @@ class MonitorStand extends DSEObject {
 
   occupiedMesh: any;
 
+  protected _slot?: Object3D;
+  protected _occupied?: Object3D;
+
+  private _boxHelper: Box3Helper;
+
   constructor() {
     super();
 
+    this._debug = true;
+    
     this.loadGLTF('monitor-stand.gltf', () => {
+      console.log(this.children);
+      this._slot = this.children.find((child) => (child.name === 'slot-0'));
+      this._occupied = this.children.find((child) => (child.name === 'occupied'));
+      console.log('occupied', this._occupied);
+      console.log('slot', this._slot);
 
+      // draw the slot
+      if (this._debug && this._slot) {
+        const helper = new Box3Helper(this.getBox(this._slot as Mesh), new Color(0x12FC4C));
+        this._boxHelper = helper;
+        this.add(helper);
+        this._boxHelper.box = this.getBox(this._slot as Mesh);
+      }
     })
+  }
+
+  /**
+   * Get the surface can be used for content object
+   * The moving area
+   * 
+   * @returns {min, max}
+   */
+  public getContainerBox() {
+    return this.getBox(this._slot as Mesh);
   }
 
   /**
@@ -21,8 +50,8 @@ class MonitorStand extends DSEObject {
    * @returns {min:Vector3, max: Vector3}
    */
   public getRestrictArea() {
-
-    const bbox = new Box3().setFromObject(this);
+    // use occupied area to calculate the restrict movement area
+    const bbox = new Box3().setFromObject(this._occupied as Mesh);
     let measure = new Vector3();
     bbox.getSize(measure);
     // console.log('size of monitor stand: ',measure);
